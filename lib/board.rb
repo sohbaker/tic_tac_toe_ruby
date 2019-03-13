220ca27ae3d334d9867c314275ee73ac50db4649
Tola's Terminal <sohbaker@gmail.com>
2019-03-13 16:04:39 +0000

    test passed: knows when the game is a tie

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,6 @@
 class Board 
-    def initialize
-        @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    def initialize(board)
+        @board = board
         @winning_combinations = [
             [0, 1, 2],
             [3, 4, 5],
@@ -11,6 +11,7 @@ class Board
             [2, 4, 6],
             [0, 4, 8]
         ]
+        @check_combinations = false
     end
 
     def print_board
@@ -33,17 +34,27 @@ class Board
         return @board[position].is_a?(Integer)
     end
 
-    def hasCurrentPlayerWon(mark)
+    def has_current_player_won(mark)
         @winning_combinations.each do | combi |
             combi.each do | cell |
                position1 = cell[0]
                position2 = cell[1]
                position3 = cell[2]
-               if@board[position1] === mark && @board[position2] === mark && @board[position2] === mark
-                return true
+               if @board[position1] === mark && @board[position2] === mark && @board[position2] === mark
+                @check_combinations = true
                end
             end
         end
-        return false
+        return @check_combinations
+    end
+
+    def game_is_a_tie
+        moves = 0
+        @board.each do | el |
+            if el.is_a?(String)
+                moves += 1
+            end
+        end
+        return moves === 9 && @check_combinations === false
     end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,7 @@ require 'board'
 
 RSpec.describe Board do
   it "prints out a new board" do
-    board = Board.new
+    board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
     expect(board.print_board).to eq("""
         1 | 2 | 3
         ---------
@@ -12,7 +12,7 @@ RSpec.describe Board do
   end
 
   it "adds a mark to a board" do
-    board = Board.new
+    board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
     expect(board.mark_board(1, 'x')).to eq("""
         x | 2 | 3
         ---------
@@ -22,30 +22,35 @@ RSpec.describe Board do
   end
 
   it "checks whether a move is valid and returns false if invalid" do
-    board = Board.new
+    board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
     board.mark_board(1, 'x')
     expect(board.is_valid_move(1)).to eq(false)
   end
 
   it "checks whether a move is valid and returns true if valid" do
-    board = Board.new
+    board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
     board.mark_board(1, 'x')
     expect(board.is_valid_move(2)).to eq(true)
   end
 
   it "knows the winning combinations for board and returns true if player has won" do
-    board = Board.new
+    board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
     board.mark_board(1, 'x')
     board.mark_board(2, 'x')
     board.mark_board(3, 'x')
-    expect(board.hasCurrentPlayerWon('x')).to eq(true)
+    expect(board.has_current_player_won('x')).to eq(true)
   end
 
   it "knows the winning combinations for board and returns false if player has not won" do
-    board = Board.new
+    board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
     board.mark_board(1, 'x')
     board.mark_board(2, 'x')
     board.mark_board(3, 'x')
-    expect(board.hasCurrentPlayerWon('o')).to eq(false)
+    expect(board.has_current_player_won('o')).to eq(false)
+  end
+
+  it "knows when the game is a tie" do
+    board = Board.new(["x", "x", "o", "o", "x", "x", "x", "o", "o"])
+    expect(board.game_is_a_tie).to eq(true)
   end
 end
