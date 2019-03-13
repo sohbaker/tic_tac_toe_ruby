d0f88fcd8c8bd035382e16ffc23072d21317045f
Tola's Terminal <sohbaker@gmail.com>
2019-03-13 15:10:35 +0000

    test passed: knows the winning combinations for board and returns true or false depending on whether player has won

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,16 @@
 class Board 
     def initialize
         @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        @winning_combinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6],
+            [0, 4, 8]
+        ]
     end
 
     def print_board
@@ -21,5 +31,19 @@ class Board
     def is_valid_move(move)
         position = move - 1
         return @board[position].is_a?(Integer)
+    end
+
+    def hasCurrentPlayerWon(mark)
+        @winning_combinations.each do | combi |
+            combi.each do | cell |
+               position1 = cell[0]
+               position2 = cell[1]
+               position3 = cell[2]
+               if@board[position1] === mark && @board[position2] === mark && @board[position2] === mark
+                return true
+               end
+            end
+        end
+        return false
     end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -21,9 +21,31 @@ RSpec.describe Board do
         7 | 8 | 9""")
   end
 
-  it "checks whether a move is valid" do
+  it "checks whether a move is valid and returns false if invalid" do
     board = Board.new
     board.mark_board(1, 'x')
     expect(board.is_valid_move(1)).to eq(false)
+  end
+
+  it "checks whether a move is valid and returns true if valid" do
+    board = Board.new
+    board.mark_board(1, 'x')
+    expect(board.is_valid_move(2)).to eq(true)
+  end
+
+  it "knows the winning combinations for board and returns true if player has won" do
+    board = Board.new
+    board.mark_board(1, 'x')
+    board.mark_board(2, 'x')
+    board.mark_board(3, 'x')
+    expect(board.hasCurrentPlayerWon('x')).to eq(true)
+  end
+
+  it "knows the winning combinations for board and returns false if player has not won" do
+    board = Board.new
+    board.mark_board(1, 'x')
+    board.mark_board(2, 'x')
+    board.mark_board(3, 'x')
+    expect(board.hasCurrentPlayerWon('o')).to eq(false)
   end
 end
