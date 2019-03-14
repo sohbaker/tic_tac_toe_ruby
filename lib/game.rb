4ca9bb3769318fda462812f51d871555904b84e9
Tola's Terminal <sohbaker@gmail.com>
2019-03-14 12:54:10 +0000

    test passed: creates a new board

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,12 @@
+require 'board'
+require 'player'
+
 class Game
-    
+    def initialize
+        @board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+
+    def show_board
+        @board.print_board
+    end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,13 @@
 require 'game'
 
 RSpec.describe Game do
-    it "" do
-
+    it "creates a new board" do
+        game = Game.new
+        expect(game.show_board).to eq("""
+        1 | 2 | 3
+        ---------
+        4 | 5 | 6
+        ---------
+        7 | 8 | 9""")
     end
 end
