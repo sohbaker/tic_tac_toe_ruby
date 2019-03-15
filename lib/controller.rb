edb110a5ec4339bd0193e2a0ea5fcf193140f263
Tola's Terminal <sohbaker@gmail.com>
2019-03-15 21:02:07 +0000

    create game controller class

---
+++ lib/controller.rb
@@ -0,0 +1,5 @@
+class Controller 
+    def initialize
+        game = Game.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+end
---
+++ spec/controller_spec.rb
@@ -0,0 +1,7 @@
+require 'controller'
+
+RSpec.describe Controller do
+    it "intialises with a new instance of a game" do
+        
+    end 
+end
