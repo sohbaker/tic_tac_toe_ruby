9733fc6143885c495b492a8f2050eff2b4efb4f4
Tola's Terminal <sohbaker@gmail.com>
2019-03-27 16:15:45 +0000

    add simplecov gem and test coverage files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+# Allows files in lib to be required and guard to run tests
+$: << File.join(File.dirname(__FILE__), "..", "lib")
+
+# creates a coverage report
 require "simplecov"
 SimpleCov.start
+
+require "board"
+require "game"
+require "player"
+require "display"
