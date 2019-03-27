61cd06d11d0a2ee624b5eadbced12549f6be86af
Tola's Terminal <sohbaker@gmail.com>
2019-03-27 16:19:20 +0000

    amend display double

--- spec/display_double.rb
+++ spec/display_double.rb
@@ -1,43 +1,23 @@
 class DisplayDouble
-  attr_reader :game, :current_player
-
-  def initialize(moves, game)
+  def initialize(moves)
     @moves = moves
-    @game = game
   end
 
-  def greet_players
-    puts "Welcome to Tic Tac Toe"
-    puts "To make a move enter the number of the position you want to play"
-    puts "Player1's mark is 'X' and Player2's mark is 'O'"
-    puts "The game will end either when a player wins by placing their mark across a row, column or diagonally, or all the positions are taken\n"
+  def prompt_player(mark)
+    print "\nPlayer #{mark}, make a move: "
   end
 
-  def prompt_player
-    print "\nPlayer #{@game.current_player}, make a move: "
-  end
-
-  # need to consider whether display should do be doing this.
   def get_move(board)
     prompt_player
     move = @moves.shift
-    while !valid?(move, board)
-      puts "Invalid move"
-      prompt_player
-      move = @moves.shift
-    end
     move
   end
 
-  def valid?(move, board)
-    board.available_moves.include?(move)
-  end
-
   def announce_tie
-    "It's a tie!"
+    puts "\nIt's a tie!"
   end
 
-  def announce_win
-    " wins!"
+  def announce_win(mark)
+    puts "\n#{mark} wins!"
   end
 end
