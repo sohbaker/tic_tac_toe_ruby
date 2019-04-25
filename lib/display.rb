06bc6ad4a2c65884eb836fd37e09737f6079a61c
Tola's Terminal <sohbaker@gmail.com>
2019-04-25 15:24:33 +0100

    remove dead code

--- lib/display.rb
+++ lib/display.rb
@@ -1,9 +1,4 @@
 class Display
-  # def initialize
-  #   @game = game
-  #   # @board = game.board.board_array
-  # end
-
   def show_board(board)
     puts "" "
     #{board[0]} | #{board[1]} | #{board[2]}
@@ -17,10 +12,6 @@ class Display
     puts "Welcome to Tic Tac Toe"
   end
 
-  # def show(computers_move)
-  #   puts computers_move
-  # end
-
   def show_rules
     puts "The game will end either when a player wins by placing their mark across a row, column or diagonally, or all the positions are taken\n"
   end
@@ -29,19 +20,9 @@ class Display
     puts "Type 'hh' to play Human vs Human. Type 'hc' to play Human vs Computer\n"
   end
 
-  # def show_players
-  #   puts "Player1's mark is 'X' and Player2's mark is 'O'"
-  # end
-
   def prompt_player(mark)
     print "\nPlayer #{mark}, make a move: "
   end
-
-  # def get_move(mark)
-  #   prompt_player(mark)
-  #   input = gets.chomp
-  #   input
-  # end
 
   def take_input
     input = gets.chomp
