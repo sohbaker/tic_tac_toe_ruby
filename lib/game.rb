023f6bad157227c8bff5c09809b2b0b6cc5cf999
Tola's Terminal <sohbaker@gmail.com>
2019-04-25 15:54:20 +0100

    launch new game method in game

--- lib/game.rb
+++ lib/game.rb
@@ -11,7 +11,13 @@ class Game
     @display = Display.new
   end
 
-  def welcome_instructions
+  def launch_new_game
+    show_welcome_instructions
+    type = get_game_type
+    create_players(type)
+  end
+    
+  def show_welcome_instructions
     @display.greet_players
     @display.show_rules
   end
@@ -19,44 +25,42 @@ class Game
   def get_game_type
     @display.choose_game_type
     type = @display.take_input
-    create_players(type)
   end
 
   def create_players(game_type)
     if game_type == "hh"
       @player1 = Human.new("X")
       @player2 = Human.new("O")
-      @current_player = @player1
     elsif game_type == "hc"
       @player1 = Human.new("X")
       @player2 = Computer.new("O", @board)
-      @current_player = @player1
     else
       @display.notify_invalid("choice")
       get_game_type()
     end
+    @current_player = @player1
   end
 
   def play_game
     until over?
-     system('clear') 
+     clear_screen()
       @display.show_board(@board.board_grid)
       @display.prompt_player(@current_player.mark)
       move = @current_player.get_move
       check_move(move)
       if !player_wins?(@current_player.mark)
-        toggle_player
+        toggle_player()
       end
     end
-    system('clear')
-    show_outcome
+    clear_screen()
+    show_outcome()
   end
 
   def check_move(move)
     if valid?(move)
       complete_move(move, @current_player.mark)
     else
-      get_new_move
+      get_new_move()
     end
   end
 
@@ -101,5 +105,9 @@ class Game
     else
       @display.announce_win(@current_player.mark)
     end
+  end
+  
+  def clear_screen
+    system('clear') 
   end
 end
