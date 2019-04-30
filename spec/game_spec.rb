9fd233121c3fff155972fb93a432a0074eac7b55
Tola's Terminal <sohbaker@gmail.com>
2019-04-30 15:00:12 +0100

    move stub display into a class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,26 +1,7 @@
 require "spec_helper"
 require "game"
 require "human_double"
-
-class StubDisplay
-  def show_board(_board)
-  end
-
-  def announce_tie
-  end
-  
-  def announce_win(player)
-  end
-
-  def clear_screen
-  end
-  
-  def prompt_player(player)
-  end
-
-  def notify_invalid(selection)
-  end
-end
+require "stub_display"
 
 RSpec.describe Game do
  it "knows that the game can continue" do
