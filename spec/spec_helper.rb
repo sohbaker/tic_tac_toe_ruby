b4f7f85f3d05edd72e89ee0d1749da0fef68b433
Tola's Terminal <sohbaker@gmail.com>
2019-05-01 13:38:52 +0100

    remove dead code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,28 +10,8 @@ require "game"
 require "display"
 require "computer"
 
-def x_wins_sequence
-  ["1", "4", "2", "5", "3"]
-end
-
-def tied_game_sequence
-  ["9", "5", "1", "6", "4", "7", "3", "2", "8"]
-end
-
-def computer_player
-  ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-end
-
 def empty_board
   ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-end
-
-def x_wins_on_next_move
-  ["O", "X", "3", "4", "5", "O", "7", "X", "9"]
-end
-
-def o_wins_on_next_move
-  ["O", "X", "3", "O", "5", "6", "7", "8", "X"]
 end
 
 def tied_game
