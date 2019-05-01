f64334fb5de229dc5ccaf1b1e3202ab486db52d5
Tola's Terminal <sohbaker@gmail.com>
2019-05-01 09:46:05 +0100

    add display argument to player module

--- spec/player_module_spec.rb
+++ spec/player_module_spec.rb
@@ -1,11 +1,12 @@
 require "player_module"
+require "stub_display"
 
 class BoardDouble 
 end
 
 RSpec.describe Player do
   it "returns two objects" do 
-    expect(Player.generate("hh", BoardDouble.new).length).to eq(2)
+    expect(Player.generate("hh", BoardDouble.new, StubDisplay.new).length).to eq(2)
   end
 end
 
