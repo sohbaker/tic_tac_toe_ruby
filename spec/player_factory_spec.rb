fea687f23ade1f6878c6cce90c1d7b6dc4b60310
Tola's Terminal <sohbaker@gmail.com>
2019-05-02 14:38:45 +0100

    fix test

--- spec/player_factory_spec.rb
+++ spec/player_factory_spec.rb
@@ -5,7 +5,7 @@ require "board_double"
 
 RSpec.describe PlayerFactory do
   it "returns two objects" do 
-    expect(Player.generate("hh", BoardDouble.new(empty_board), StubDisplay.new).length).to eq(2)
+    expect(PlayerFactory.generate("hh", BoardDouble.new(empty_board), StubDisplay.new).length).to eq(2)
   end
 end
 
