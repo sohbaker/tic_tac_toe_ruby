ea90e63d942beb052f013b264ad387ea25d20302
Tola's Terminal <sohbaker@gmail.com>
2019-05-02 17:33:10 +0100

    use ternary operator

--- lib/player_factory.rb
+++ lib/player_factory.rb
@@ -4,11 +4,6 @@ require "game_type"
 
 module PlayerFactory
   def self.generate(type, board, display)
-    if type == GameType::HUMANHUMAN 
-      players = [Human.new("X", display), Human.new("O", display)]
-    elsif type == GameType::HUMANCOMPUTER 
-      players = [Human.new("X", display), Computer.new("O", board, display)]
-    end
-    players
+    type == GameType::HUMANHUMAN ? [Human.new("X", display), Human.new("O", display)] : [Human.new("X", display), Computer.new("O", board, display)]
   end
 end
