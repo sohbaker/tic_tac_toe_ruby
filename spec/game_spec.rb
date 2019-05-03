9087f8bbffafcf1bbb87bf960f06d796b71eaf0c
Tola's Terminal <sohbaker@gmail.com>
2019-05-03 11:03:15 +0100

    rewrite tests, dependency more apparent

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,26 +2,35 @@ require "spec_helper"
 require "game"
 require "human_double"
 require "stub_display"
+require "board"
+require "computer"
 
 RSpec.describe Game do
- it "knows that the game can continue" do
-   game = Game.new(Board.new, StubDisplay.new, HumanDouble.new("X", ["1"]), HumanDouble.new("O", ["2"]))
-   expect(game.over?).to eq(false)
+  let(:board) { Board.new }
+  let(:display) { StubDisplay.new }
+
+  it "knows that the game can continue" do
+    moves = [["1"], ["2"]]
+    game = Game.new(board, display, *[HumanDouble.new("X", moves.shift), HumanDouble.new("O", moves.shift)])
+
+    expect(game.over?).to eq(false)
   end
 
- it "receives a message when the game ends in a tie" do 
-   game = Game.new(Board.new, StubDisplay.new, HumanDouble.new("X", ["9","1","4","3","8"]), HumanDouble.new("O", ["5","6","7","2"]))
-   allow(game).to receive(:show_outcome)
+  it "receives a message when the game ends in a tie" do 
+    moves = [["9","1","4","3","8"], ["5","6","7","2"]]
+    game = Game.new(board, display, *[HumanDouble.new("X", moves.shift), HumanDouble.new("O", moves.shift)])
+    allow(game).to receive(:show_outcome)
 
-   game.play_game
-   expect(game).to have_received(:show_outcome)
- end
+    game.play_game
+    expect(game).to have_received(:show_outcome)
+  end
 
- it "receives a message when the game ends with a win" do 
-   game = Game.new(Board.new, StubDisplay.new, HumanDouble.new("X", ["1","4","7"]), HumanDouble.new("O", ["2","3"]))
-   allow(game).to receive(:show_outcome)
+  it "receives a message when the game ends with a win" do 
+    moves = [["1","4","7"], ["2","3"]]
+    game = Game.new(board, display, *[HumanDouble.new("X", moves.shift), HumanDouble.new("O", moves.shift)])
+    allow(game).to receive(:show_outcome)
 
-   game.play_game
-   expect(game).to have_received(:show_outcome)
- end
+    game.play_game
+    expect(game).to have_received(:show_outcome)
+  end
 end
